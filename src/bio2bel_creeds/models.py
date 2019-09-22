8a6cc2e6e083422d3bf7d7a1193a1774c5567bdf
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-09-22 23:27:40 +0200

    Add mappings to PyBEL

--- src/bio2bel_creeds/models.py
+++ src/bio2bel_creeds/models.py
@@ -8,6 +8,9 @@ from sqlalchemy import Column, Float, ForeignKey, Integer, String, Text
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import relationship
 
+import pybel.dsl
+from pybel import BELGraph
+from pybel.constants import CITATION_REFERENCE, CITATION_TYPE
 from .constants import MODULE_NAME
 
 __all__ = [
@@ -39,6 +42,36 @@ class SingleGenePerturbationExperiment(Base):
     gene_id = Column(String(255), nullable=False, doc='The identifier of the gene that was perturbed')
     gene_symbol = Column(String(255), nullable=False, doc='The gene symbol of the gene that was perturbed')
 
+    def as_bel(self) -> pybel.dsl.Protein:
+        """Get the PyBEL node representing the gene perturbed in this experiment."""
+        return pybel.dsl.Protein(
+            namespace=self.gene_namespace,
+            identifier=self.gene_id,
+            name=self.gene_symbol,
+        )
+
+    def add_to_graph(self, graph: BELGraph):
+        """Add the results from this experiment to the graph."""
+        source = self.as_bel()
+        citation = {
+            CITATION_TYPE: 'curie',
+            CITATION_REFERENCE: f'geo:{self.geo_id}',
+        }
+        evidence = 'Experimental'
+
+        # TODO add more mappings
+        perturbation_to_direction_to_relation = dict(
+            knockout=dict(up=graph.add_decreases, down=graph.add_increases),
+            overexpression=dict(up=graph.add_increases, down=graph.add_decreases),
+        )
+        default_direction_to_relation = dict(up=graph.add_regulates, down=graph.add_regulates)
+        direction_to_relation = perturbation_to_direction_to_relation.get(self.pert_type, default_direction_to_relation)
+
+        for single_gene_perturbation in self.single_gene_perturbations:
+            target = single_gene_perturbation.as_bel()
+            fn = direction_to_relation[single_gene_perturbation.direction]
+            fn(source, target, citation=citation, evidence=evidence)
+
 
 class SingleGenePerturbation(Base):
     """Represents a single gene perturbation."""
@@ -49,10 +82,16 @@ class SingleGenePerturbation(Base):
     single_gene_perturbation_experiment_id = Column(
         Integer, ForeignKey(f'{SingleGenePerturbationExperiment.__tablename__}.id'), nullable=False,
     )
-    single_gene_perturbation_experiment = relationship(SingleGenePerturbationExperiment)
+    single_gene_perturbation_experiment = relationship(
+        SingleGenePerturbationExperiment, backref='single_gene_perturbation',
+    )
 
     gene_namespace = Column(String(255), nullable=False)
     gene_id = Column(String(255), nullable=False, doc='The identifier of the gene that was measured')
     gene_symbol = Column(String(255), nullable=False, doc='The gene symbol of the gene that was measured')
     direction = Column(String(4), nullable=False)
     value = Column(Float, nullable=False)
+
+    def as_bel(self) -> pybel.dsl.Rna:
+        """Get the PyBEL node representing the gene measured in this experiment."""
+        return pybel.dsl.Rna(namespace=self.gene_namespace, identifier=self.gene_id, name=self.gene_symbol)
